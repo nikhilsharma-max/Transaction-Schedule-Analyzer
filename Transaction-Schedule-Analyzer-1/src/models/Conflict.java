3971442560fb019d2f06bf5fb44de5af7e25014c
Nikhil Sharma <nikhilsharma745408@gmail.com>
2026-06-08 10:52:51 +0530

    Completed upto operation.java models

---
+++ Transaction-Schedule-Analyzer-1/src/models/Conflict.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("This is a conflict example.");
+    }
+}
---
+++ Transaction-Schedule-Analyzer-1/src/models/Operation.java
@@ -0,0 +1,41 @@
+package models;
+
+public class Operation {
+    private OperationType operationType;
+    private int transactionId;
+    private String dataItem;
+
+    public Operation(OperationType operationType, int transactionId, String dataItem) {
+        this.operationType = operationType;
+        this.transactionId = transactionId;
+        this.dataItem = dataItem;
+    }
+
+    // Getters and setters  -- Setters are commented out to make the Operation class immutable
+    public OperationType getOperationType() {
+        return operationType;
+    }   
+    // public void setOperationType(OperationType operationType) {
+    //     this.operationType = operationType;
+    // }
+    public int getTransactionId() {
+        return transactionId;
+    }
+    // public void setTransactionId(int transactionId) {
+    //     this.transactionId = transactionId; 
+    // }
+    public String getDataItem() {
+        return dataItem;
+    }
+    // public void setDataItem(String dataItem) {
+    //     this.dataItem = dataItem;
+    // }
+    @Override
+    public String toString() {
+        return "Operation{" +
+                "operationType=" + operationType +
+                ", transactionId=" + transactionId +
+                ", dataItem='" + dataItem + '\'' +
+                '}';
+    }
+}
